b499443cc8b42cfe4b826954936fe0dfa25afca0
Apichat Saekrung <apichat.s@cupcodeteam.com>
2017-11-17 14:02:38 +0700

    Update detail

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import * as axios from 'axios'
+import _ from 'lodash'
 const express = require('express');
 const line = require('@line/bot-sdk');
 
@@ -11,6 +13,46 @@ const config = {
 };
 
 const client = new line.Client(config);
+
+const listRecommend = () => {
+    let data = {}
+    this.search('funds', 'homeList', {}, {
+      page: 0,
+      size: 6,
+      sort: "fundResult.sweightTotal,DESC"
+    }, 'fundList').then(resp => {
+      data = resp.data._embedded
+    }).catch(err => {
+      console.error(err)
+    })
+
+  return data
+}
+
+
+axios.defaults.baseURL = 'https://treasurist.com/api';
+axios.defaults.headers.common['TR-Device-Type'] = 'Web'
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response;
+
+  }, function (error) {
+    // Do something with response error
+    console.log("axios interceptor error: ", error.status)
+    if(error.status === 404){
+      return Promise.reject('404 Not found')
+    }
+    return Promise.reject(error);
+  });
 
 app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
@@ -34,7 +76,52 @@ function handleMessageEvent(event) {
         text: 'สวัสดีครับยินดีต้อนรับ'
     };
 
+    var eventText = event.message.text.toLowerCase();
+
+    if (eventText === "about you") {
+        let data = listRecommend()
+        msg = {
+            "type": "template",
+            "altText": "Welcome to Treasurist",
+            "template": {
+                "type": "buttons",
+                "thumbnailImageUrl": "https://www.treasurist.com/assets/images/logo-large.png",
+                "title": "Menu",
+                "text": "Please select",
+                "actions": [
+                    {
+                      "type": "uri",
+                      "label": "",
+                      "uri": "https://www.treasurist.com/suitabilityTest"
+                    },
+                    {
+                      "type": "uri",
+                      "label": "Search",
+                      "uri": "https://www.treasurist.com/search?riskLevel=&taxBenefit=-1&location=-1&keyword=&sort=fundResult.sweightTotal,DESC"
+                    },
+                    {
+                      "type": "uri",
+                      "label": "View detail",
+                      "uri": "https://www.treasurist.com/howItWork"
+                    }
+                ]
+                
+            }
+        }
+    } else if (eventText === "top") {
+        let data = listRecommend()
+        var msg = {
+            type: 'text',
+            text: {data}
+        };
+    }
+
     return client.replyMessage(event.replyToken, msg);
+}
+
+function search(resource, method, query, pageable, projection){
+    let params = _.assign({}, pageable, {projection}, query)
+    return axios.get(resource+'/search/'+method, {auth, params})
 }
 
 app.set('port', (process.env.PORT || 5000));
