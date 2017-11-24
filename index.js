808be872d48e7da9dff1268526c8099646c26679
Apichat Saekrung <apichat.s@cupcodeteam.com>
2017-11-24 16:16:27 +0700

    check

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 const axios = require('axios');
 const moment = require('moment');
+const question = require('./src/testResulte/Questionair')
+
 require('dotenv').config();
 
 const app = express();
@@ -14,25 +16,34 @@ const config = {
 const client = new line.Client(config);
 
 app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
+    if (!validate_signature(req.headers['x-line-signature'], req.body)) {
+        return;
+    } else {
+
+        Promise.all(req.body.events.map(handleEvent))
+            .then((result) => res.json(result));
+    }
 });
+
+function validate_signature(signature, body) {
+    return signature == crypto.createHmac('sha256', config.channelSecret).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+}
 
 function handleEvent(event) {
 
     console.log(event);
     if (event.type === 'message' && event.message.type === 'text') {
         handleMessageEvent(event);
+    } else if (event.type === postback) {
+        handlePostBackEvent(event);
     } else {
         return Promise.resolve(null);
     }
 }
 
 function handleMessageEvent(event) {
-
     var eventText = event.message.text.toLowerCase()
-    
+    var re = /(\bsearch\b)/;
     if (eventText === "about you") {
         let msg = {
             "type": "template",
@@ -44,54 +55,72 @@ function handleMessageEvent(event) {
                 "text": "Please select",
                 "actions": [
                     {
-                      "type": "uri",
-                      "label": "Test",
-                      "uri": "https://www.treasurist.com/suitabilityTest"
+                        "type": "uri",
+                        "label": "Test",
+                        "uri": "https://www.treasurist.com/suitabilityTest"
                     },
                     {
-                      "type": "uri",
-                      "label": "Search",
-                      "uri": "https://www.treasurist.com/search?riskLevel=&taxBenefit=-1&location=-1&keyword=&sort=fundResult.sweightTotal,DESC"
+                        "type": "uri",
+                        "label": "Search",
+                        "uri": "https://www.treasurist.com/search?riskLevel=&taxBenefit=-1&location=-1&keyword=&sort=fundResult.sweightTotal,DESC"
                     },
                     {
-                      "type": "uri",
-                      "label": "View detail",
-                      "uri": "https://www.treasurist.com/howItWork"
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "https://www.treasurist.com/howItWork"
                     }
                 ]
-                
+
             }
         }
         return client.replyMessage(event.replyToken, msg);
-    } else {
-        var keyword = eventText.split("search").join("")
-        
-        axios.get(`http://treasurist.com/api/funds/search/main?page=0&size=9&sort=fundResult.sweightTotal,DESC&projection=fundList&riskLevel=1,2,3,4,5,6,7,8&taxBenefit=0,1&location=1,2&keyword=%25${keyword}%25`)
-          .then(response => {
-            let data = response.data._embedded.funds
-            console.log("Data size > ",  data != undefined && data.length)
-            let msg =  data != undefined ? resultList(data) : {
-                "type": "text",
-                "text": "Search Not Found!, Please Try Again."
+    } else if (eventText === "test") {
+        let msg = {
+            "type": "template",
+            "altText": "Test open port",
+            "template": {
+                "type": "buttons",
+                "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+                "title": "เริ่มวงแผนการลงทุนที่เหมาะกับคุณ",
+                "actions": [
+                    {
+                        "type": "postback",
+                        "label": "Start",
+                        "data": "action=test"
+                    }
+                ]
             }
-            return client.replyMessage(event.replyToken, msg);
-          })
-          .catch(error => {
-            console.log(error);
-          });
+        }
+        return client.replyMessage(event.replyToken, msg);
+    } else if (re.test(eventText)) {
+        var keyword = eventText.split("search").join("")
+
+        axios.get(`http://treasurist.com/api/funds/search/main?page=0&size=9&sort=fundResult.sweightTotal,DESC&projection=fundList&riskLevel=1,2,3,4,5,6,7,8&taxBenefit=0,1&location=1,2&keyword=%25${keyword}%25`)
+            .then(response => {
+                let data = response.data._embedded.funds
+                console.log("Data size > ", data != undefined && data.length)
+                let msg = data != undefined ? resultList(data) : {
+                    "type": "text",
+                    "text": "Search Not Found!, Please Try Again."
+                }
+                return client.replyMessage(event.replyToken, msg);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 }
 
 function resultList(data) {
-    let resultList = (data !== null ||  data !== undefined) && {
+    let resultList = (data !== null || data !== undefined) && {
         "type": "template",
         "altText": "this is a carousel template",
         "template": {
             "type": "carousel",
-            "columns": data.map( s => {
+            "columns": data.map(s => {
                 return {
                     "thumbnailImageUrl": "https://www.treasurist.com/assets/images/logo-large.png",
-                    "title": `${s.fundCode} :: ${s.lastestNavDateList[0].nav ? s.lastestNavDateList[0].nav : '0.0000'} (Baht/Unit)`, 
+                    "title": `${s.fundCode} :: ${s.lastestNavDateList[0].nav ? s.lastestNavDateList[0].nav : '0.0000'} (Baht/Unit)`,
                     "text": `${s.fundNameTh}`,
                     "actions": [
                         {
@@ -100,12 +129,39 @@ function resultList(data) {
                             "uri": `http://www.treasurist.com/funds/${s.fundId}/${s.fundNameEn.split(/[\s/@+.()%]/).join('-').toLowerCase()}`
                         }
                     ]
-                  }
+                }
             })
         }
-      }
-    
+    }
+
     return resultList
+}
+
+function handleMessageEvent(event) {
+    var eventPostback = event.postback.data.split("&")
+    var eventPostbackAction = eventPostback[0] != undefined && eventPostback[0].split("=")[1]
+    var eventPostBackItem = eventPostback[1] != undefined ? parseInt(eventPostback[1].split("=")[1]) : 1
+
+    if (eventPostbackAction === "test" && eventPostBackItem <= 16) {
+        let msg = {
+            "type": "template",
+            "altText": question[eventPostBackItem].altQuestion,
+            "template": {
+                "type": "buttons",
+                "title": question[eventPostBackItem].question,
+                "actions": [
+                    question[eventPostBackItem].question.choices.map( c => {
+                        return {
+                            "type": "postback",
+                            "label": c.text,
+                            "data": `action=test&itemid=${eventPostBackItem+1}&value=${c.value}`
+                        }
+                    })
+                ]
+            }
+        }
+        return client.replyMessage(event.replyToken, msg);
+    } 
 }
 
 app.set('port', (process.env.PORT || 5000));
