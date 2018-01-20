cebe825e2d69b73172b92573502550aa413317b4
Apichat Saekrung <apichat.s@cupcodeteam.com>
2018-01-21 01:16:56 +0700

    update

--- index.js
+++ index.js
@@ -395,9 +395,9 @@ function handleSearchEvent(event) {
     if (searchPostbackAction === "search" && searchPostBackItem < 2) {
         console.log("here")
         let step = searchPostBackItem + 1
-        let newResult = getSearchObj((searchPostBackItem - 1), searchPostBackItemValue)
+        let newResult = getSearchObj((searchPostBackItem), searchPostBackItemValue)
         searchResult = newResult != undefined ? `${searchResult}&${newResult}` : undefined
-        let msg =  searchFilterOption(searchFilter[searchPostBackItem], searchPostBackItem)
+        let msg =  searchFilterOption(searchFilter[searchPostBackItem + 1], step)
         currentStep =  searchPostBackItem
         return client.replyMessage(event.replyToken, msg);
         
