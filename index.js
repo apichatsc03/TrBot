02c1a0db6c73739ef55384216ef51214cd32f9c1
Apichat Saekrung <apichat.s@cupcodeteam.com>
2018-01-21 01:45:23 +0700

    333

--- index.js
+++ index.js
@@ -391,13 +391,13 @@ function handleSearchEvent(event) {
     var searchPostback = event.postback != undefined ? event.postback.data.split("&") : undefined;
     var searchPostbackAction = searchPostback ? searchPostback[0] != undefined && searchPostback[0].split("=")[1] : "search"
     var searchPostBackItemValue = searchPostback ? searchPostback[2] != undefined ? parseInt(searchPostback[2].split("=")[1]) : undefined : event.message.text.toLowerCase()
-    var searchPostBackItem = searchPostback ? (searchPostback[1] != undefined ? parseInt(searchPostback[1].split("=")[1]) : 0 ): currentStep + 1 ;
+    var searchPostBackItem = searchPostback ? (searchPostback[1] != undefined ? parseInt(searchPostback[1].split("=")[1]) + 1 : 0 ): currentStep + 1 ;
     if (searchPostbackAction === "search" && searchPostBackItem < 2) {
         console.log("here")
-        let newResult = getSearchObj((searchPostBackItem), searchPostBackItemValue)
+        let newResult = getSearchObj((searchPostBackItem - 1), searchPostBackItemValue)
         searchResult = newResult != undefined ? `${searchResult}&${newResult}` : undefined
         let msg =  searchFilterOption(searchFilter[searchPostBackItem], searchPostBackItem)
-        currentStep =  searchPostBackItem + 1 
+        currentStep =  searchPostBackItem
         return client.replyMessage(event.replyToken, msg);
         
     } else if (searchPostbackAction === "search" && searchPostBackItem === 2) {
