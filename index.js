e53cf8501715916a6f9f0659ed9429edf141c988
Apichat Saekrung <apichat.s@cupcodeteam.com>
2018-01-22 23:49:58 +0700

    Update

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ let testResult;
 let currentQuestion;
 let searchResult;
 let currentStep;
-let titleMaxChar = 40;
+let titleMaxChar = 20;
 let textMaxChar	= 60;
  
 app.post('/webhook', line.middleware(config), (req, res) => {
@@ -486,7 +486,7 @@ function searchFilterOption(data, step) {
 
 function doSubmitSearch(data, event) {
     console.log("Date URL", data)
-    axios.get(`https://treasurist.com/api/funds/search/main?page=0&size=9&sort=fundResult.sweightTotal,DESC&projection=fundList&riskLevel=1,2,3,4,5,6,7,8&taxBenefit=0,1&location=1,2&keyword=%25ltf%25`)
+    axios.get(data)
         .then(response => {
             let data = response.data._embedded.funds
             let msg = data != undefined ? resultList(data) : {
